ff640a45b4ee4508227e9d60966db40d49ddf4a8
waibaBikash <kavrelibikash@gmail.com>
2024-09-30 21:44:30 +1000

    create a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,12 @@ import Cart from './pages/Cart'
 import Login from './pages/Login'
 import PlaceOrder from './pages/PlaceOrder'
 import Orders from './pages/Orders'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className='px-4 sm:px-[5vw] lg:px-[9vh]'>
+      <Navbar/>
       <Routes>
          <Route path='/' element={<Home/>}/>
          <Route path='/collection' element={<Collection/>}/>
