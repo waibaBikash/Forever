376be83c34238aafef27a21ee95bb5c6dc5b872e
waibaBikash <kavrelibikash@gmail.com>
2024-09-30 22:13:05 +1000

    complete navebar functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,8 +51,25 @@ const Navbar = () => {
                     <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt="" />
         </div>
          {/* Sidebar menu for small screens */}
-         <div className='absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all'>
-
+         <div className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible ? 'w-full' : 'w-0'}`}>
+              <div className='flex flex-col text-gray-600'>
+                 <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
+                   <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
+                   <p>Back</p>
+                 </div>
+                   <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/'>
+                     HOME
+                   </NavLink>
+                   <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/collection'>
+                     COLLECTION
+                   </NavLink>
+                   <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/about'>
+                     ABOUT
+                   </NavLink>
+                   <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to='/contact'>
+                     CONTACT
+                   </NavLink>
+              </div>
          </div>
 
     </div>
