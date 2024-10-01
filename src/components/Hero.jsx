24d0fd0e55f0c549ccc8ab3ad25611da76109fd9
waibaBikash <kavrelibikash@gmail.com>
2024-10-01 20:31:52 +1000

    complete hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const Hero = () => {
   return (
@@ -10,13 +11,15 @@ const Hero = () => {
                <p className='w-8 md:w-11 h-[2px] bg-[#414141]'></p>
                <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
              </div>
-               <h1 className='text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Lastest Arrivals</h1>
+               <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>Lastest Arrivals</h1>
                 <div className='flex items-center gap-2'>
                     <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
                     <p className='w-8 md:w-11 h-[1px] bg-[#414141]'></p>
                 </div>
           </div>
       </div>
+        {/* Hero Right side */}
+        <img className='w-full sm:w-1/2' src={assets.hero_img} alt="" />
     </div>
   )
 }
