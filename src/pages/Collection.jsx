96ba84c9fae2f7f442d6f27a73a1088b5e1b3f70
waibaBikash <kavrelibikash@gmail.com>
2024-10-04 00:00:08 +1000

    search functionality

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -6,7 +6,7 @@ import ProductItem from '../components/ProductItem';
 
 const Collection = () => {
 
-  const { products } = useContext(ShopContext);
+  const { products, search, showSearch } = useContext(ShopContext);
   const [showFilter,setShowfilter] = useState(false);
   const [filterProducts,setFilterProducts] = useState([]);
   const [category,setCategory] = useState([]);
@@ -31,6 +31,9 @@ const Collection = () => {
 
   const applyFilter = () => {
     let productsCopy = products.slice();
+    if (showSearch && search) {
+      productsCopy = productsCopy.filter(item=> item.name.toLowerCase().includes(search.toLowerCase()))
+    }
 
     if (category.length > 0) {
       productsCopy = productsCopy.filter(item => category.includes(item.category));
@@ -64,7 +67,7 @@ const Collection = () => {
 
   useEffect(() => {
     applyFilter(products)
-  },[category,subCategory])
+  },[category,subCategory,search,showSearch])
 
   useEffect(()=>{
     sortProduct();
