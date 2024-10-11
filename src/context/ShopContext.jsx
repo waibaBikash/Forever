70319647c9ee51ee3765ab6873604204b03d5c88
waibaBikash <kavrelibikash@gmail.com>
2024-10-11 20:20:27 +1100

    checkout button functionality

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
   const [search,setSerach] = useState('');
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
+  const navigate = useNavigate();
 
 
   const addToCart = async (itemId, size) => {
@@ -84,7 +86,7 @@ const ShopContextProvider = (props) => {
   const value = {
      products, currency, delivery_fee,
      search,setSerach,showSearch,setShowSearch, cartItems, addToCart,
-     getCartCount,updateQuantity,getCartAmount
+     getCartCount,updateQuantity,getCartAmount, navigate
   }
   return(
     <ShopContext.Provider value={value}>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,7 @@ import CartTotal from '../components/CartTotal';
 
 const Cart = () => {
 
-  const { products, currency, cartItems, updateQuantity } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity, navigate } = useContext(ShopContext);
 
   const [cartData, setCartData] = useState([]);
 
@@ -63,6 +63,9 @@ const Cart = () => {
          <div className='flex justify-end my-20'>
              <div className='w-full sm:w-[450px]'>
                 <CartTotal/>
+                 <div className='w-full text-end'>
+                     <button onClick={()=>navigate('/place-order')} className='bg-black text-white text-sm my-8 px-8 py-3'>PROCEED TO CHECKOUT</button>
+                 </div>
              </div>
          </div>
     </div>
