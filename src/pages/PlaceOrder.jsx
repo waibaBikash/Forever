df1d19522df800fe53f7950c46cccec7dfed82b6
waibaBikash <kavrelibikash@gmail.com>
2024-10-11 22:36:41 +1100

    create a placOrder page

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Title from '../components/Title';
 
 const PlaceOrder = () => {
   return (
-    <div>PlaceOrder</div>
+    <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
+      {/*................. Left Side ............ */}
+       <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
+          <div className='text-xl sm:text-2xl my-3'>
+            <Title text1={'DELIVERY'} text2={'INFORMATION'} />
+          </div>
+            <div className='flex gap-3'>
+               <input className='border border-gray-300 roudded py-1.5 px-3.5 w-full' type="text" placeholder='First name' />
+               <input className='border border-gray-300 roudded py-1.5 px-3.5 w-full' type="text" placeholder='Last name' />
+            </div>
+       </div>
+    </div>
   )
 }
 
